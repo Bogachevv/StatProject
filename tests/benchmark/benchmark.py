2f19123d0fb747c96dfd4a1a4a5ee6c3ffe3070b
Bogachevv <dog67688@gmail.com>
2023-04-14 00:06:35 +0300

    Wrote tests and benchmark

--- tests/benchmark/benchmark.py
+++ tests/benchmark/benchmark.py
@@ -40,28 +40,28 @@ def write_init(path: str, method: str, units: typing.Literal['s', 'ms', 'us', 'n
 def main():
     rep_c = 10
 
-    write_init(r"bench_res/fast_precision.txt", method="fast_precision", units="ms")
-    write_init(r"bench_res/naive_precision.txt", method="naive_precision", units="ms")
-    write_init(r"bench_res/kde_precision.txt", method="kde_precision", units="ms")
+    write_init(r"benchmark/bench_res/fast_precision.txt", method="fast_precision", units="ms")
+    write_init(r"benchmark/bench_res/naive_precision.txt", method="naive_precision", units="ms")
+    write_init(r"benchmark/bench_res/kde_precision.txt", method="kde_precision", units="ms")
 
     warm_up(fast_precision)
     warm_up(kde_precision)
     warm_up(naive_precision)
 
-    steps_c = 15
-    for step, size in enumerate(map(int, np.linspace(25, 1_000, endpoint=True, num=steps_c))):
+    steps_c = 50
+    for step, size in enumerate(map(int, np.linspace(25, 3_000, endpoint=True, num=steps_c))):
         y_act = np.linspace(0, 1, num=size)
         y_pred = np.linspace(0, 1, num=size) + stats.norm.rvs(loc=0.0, scale=0.3, size=size)
         print(f"----- {size=}\tstep {step+1}\\{steps_c} -----")
         mean, std = sample(fast_precision, y_pred, y_act, [0.05, 0.5, 0.95], rep_c=rep_c*10)
         print(f"\tFast_precision:   {mean * 1000:.2f} +/- {std * 1000:.2f} ms")
-        write_rec(size, mean, std, r"bench_res/fast_precision.txt")
+        write_rec(size, mean, std, r"benchmark/bench_res/fast_precision.txt")
         mean, std = sample(naive_precision, y_pred, y_act, [0.05, 0.5, 0.95], rep_c=rep_c*10)
         print(f"\tNative_precision: {mean * 1000:.2f} +/- {std * 1000:.2f} ms")
-        write_rec(size, mean, std, r"bench_res/naive_precision.txt")
+        write_rec(size, mean, std, r"benchmark/bench_res/naive_precision.txt")
         mean, std = sample(kde_precision, y_pred, y_act, [0.05, 0.5, 0.95], rep_c=rep_c)
         print(f"\tKDE_precision:    {mean * 1000:.2f} +/- {std * 1000:.2f} ms")
-        write_rec(size, mean, std, r"bench_res/kde_precision.txt")
+        write_rec(size, mean, std, r"benchmark/bench_res/kde_precision.txt")
 
 
 if __name__ == '__main__':
