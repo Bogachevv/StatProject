dfeb680b6981ec078ebb4f14942f4945997fe84e
Bogachevv <dog67688@gmail.com>
2023-09-23 21:44:10 +0300

    Rewrite using_kde.plot_precision

--- using_kde/precision_recall.py
+++ using_kde/precision_recall.py
@@ -1,103 +1,103 @@
+__all__ = ['plot_precision', 'plot_recall']
+
+
 import numpy as np
+from numpy import ndarray
 from matplotlib import pyplot as plt
 from typing import Literal, Callable, Tuple
 from scipy import stats, integrate
 from bisect import bisect_right
 
 
-def extend(arr: np.array, gen: Callable[[float, float, int], float]) -> np.array:
-    new_arr = np.zeros(arr.shape[0] * 2 - 1)
-    for i in range(arr.shape[0] - 1):
-        new_arr[2*i] = arr[i]
-        new_arr[2*i + 1] = gen(arr[i], arr[i+1], i)
-    new_arr[-1] = arr[-1]
-
-    assert new_arr.shape[0] == 2*arr.shape[0] - 1
-    return new_arr
+def _pack(val, min_v=0, max_v=1):
+    return min(max(val, min_v), max_v)
 
 
-def get_cdf(pdf, left: float, right: float, n: int = 7, int_eps: float = 0.01, arg_delta: float = 0.01) -> Tuple[np.array, np.array]:
-    arg_sp_gen = lambda a_prev, a_next, i: (a_prev + a_next) / 2
-
-    old_arg_sp = np.linspace(left, right, num=n)
-    new_arg_sp = extend(old_arg_sp, arg_sp_gen)
-    old_val_sp = np.array([pdf(x) for x in old_arg_sp]).flatten()
-    # old_val_sp = pdf(old_arg_sp)
-    new_val_sp = extend(old_val_sp, lambda a_prev, a_next, i: pdf(new_arg_sp[2 * i + 1]))
-    old_int = integrate.cumulative_trapezoid(old_val_sp, old_arg_sp, initial=0)
-    new_int = integrate.cumulative_trapezoid(new_val_sp, new_arg_sp, initial=0)
-
-    while True:
-        delta_int = max(abs(new_int[2*i] - old_int[i]) for i in range(n))
-        net_resolution = (new_arg_sp[-1] - new_arg_sp[0]) / len(new_arg_sp)
-        arg_eps = arg_delta / max(new_val_sp)
-        if (delta_int < int_eps) and (net_resolution < arg_eps):
-            break
-        n = 2 * n - 1
-        old_arg_sp = new_arg_sp.copy()
-        # new_arg_sp = extend(old_arg_sp, arg_sp_gen)
-        new_arg_sp = np.linspace(left, right, num=2*n - 1)
-        old_val_sp = new_val_sp.copy()
-        new_val_sp = extend(old_val_sp, lambda a_prev, a_next, i: pdf(new_arg_sp[2 * i + 1]))
-        old_int = new_int.copy()
-        new_int = integrate.cumulative_trapezoid(new_val_sp, new_arg_sp, initial=0)
-
-    # print(n)
-    assert new_int[-1] > 0
-    cdf = new_int / new_int[-1]
-
-    return new_arg_sp, cdf
-
-
-def pack(val, min=0, max=1):
-    if val < min:
-        return min
-    if val > max:
-        return max
-
-    return val
-
-
-def get_quantile(cdf, u, q):
+def _get_quantile(cdf: ndarray, u: ndarray, q: float):
     q_pos = bisect_right(cdf, q)
-    q_pos = pack(q_pos, min=0, max=u.shape[0] - 1)
+    q_pos = _pack(q_pos, min_v=0, max_v=u.shape[0] - 1)
     return u[q_pos]
 
 
-def plot_precision(y_act: np.array, y_pred: np.array, quantiles: list[float] = None,
+def _prod(a: ndarray, b: ndarray) -> ndarray:
+    r"""
+    Returned array ordered by a
+    :return: :math:`a \times b = [(x, \, y) \: | \: (x \in a) \wedge (y \in b)\]`
+    """
+    return np.vstack([np.repeat(a, len(b)), np.tile(b, len(a))])
+
+
+def _calc_mean(pdf: ndarray, u: ndarray) -> float:
+    return integrate.trapezoid(pdf*u, u)
+
+
+def _sort_pair(x: ndarray, y: ndarray) -> None:
+    """
+    Sort arrays x, y by x-order
+    :param x: 1D array, MUTABLE
+    :param y: 1D array, MUTABLE
+    :return:
+    """
+    ast = np.argsort(x)
+    x[:] = x[ast]
+    y[:] = y[ast]
+
+
+def _get_plotter(plotter_mode: Literal['plot', 'scatter'], ax) -> Callable:
+    if plotter_mode == 'plot':
+        return ax.plot
+    if plotter_mode == 'scatter':
+        return ax.scatter
+    raise NotImplemented
+
+
+def _get_modifier(mode: Literal['raw', 'subtraction', 'division'], param: ndarray | None) -> Callable:
+    if mode == 'raw':
+        return lambda x: x
+    if mode == 'subtraction':
+        return lambda x: x - param
+    if mode == 'division':
+        assert np.all(param != 0.0)
+        return lambda x: x / param
+    raise NotImplemented
+
+
+def plot_precision(y_act: np.ndarray, y_pred: np.ndarray, quantiles: list[float] = None,
                    plt_mode: Literal['raw', 'subtraction', 'division'] = 'raw',
                    plotter_mode: Literal['plot', 'scatter'] = 'plot',
-                   ax: plt.axes = None) -> None:
+                   ax: plt.axes = None,
+                   quality: int = 1_000,
+                   resolution: int = 100) -> None:
     quantiles = [0.05, 0.5, 0.95] if quantiles is None else quantiles
     ax = plt.axes() if ax is None else ax
 
-    if plotter_mode == 'plot':
-        ast = np.argsort(y_pred)
-        y_act = y_act[ast]
-        y_pred = y_pred[ast]
+    pred_sp = np.linspace(np.min(y_pred), np.max(y_pred), num=resolution)
+    quantiles_sp = np.zeros((len(quantiles), pred_sp.shape[0]))
+    mean_sp = np.zeros_like(pred_sp)
 
-    kde = stats.gaussian_kde(np.vstack([y_act, y_pred]))
-    quantiles_sp = np.zeros((len(quantiles), len(y_pred)))
+    p = _prod(pred_sp, np.linspace(np.min(y_act), np.max(y_act), num=quality))
+    kde_evs = (stats.
+               gaussian_kde(np.vstack([y_act, y_pred])).
+               evaluate(p))
 
-    left_bound = min(y_act)
-    right_bound = max(y_act)
-    for j, pred in enumerate(y_pred):
-        u, cdf = get_cdf(lambda x: kde((x, pred))[0], left_bound, right_bound,
-                         arg_delta=min(min(quantiles), 1 - max(quantiles)), int_eps=1e-3)
+    for j, pred in enumerate(pred_sp):
+        u = p[1, p[0] == pred]
+        cdf = integrate.cumulative_trapezoid(kde_evs[p[0] == pred], u, initial=0.0)
+        mean_sp[j] = _calc_mean(kde_evs[p[0] == pred] / cdf[-1], u)
+        cdf /= cdf[-1]
+
         for i, q in enumerate(quantiles):
-            quantiles_sp[i, j] = get_quantile(cdf, u, q)
+            quantiles_sp[i, j] = _get_quantile(cdf, u, q)
 
-    modifier = {'raw': (lambda x: x),
-                'subtraction': (lambda x: x - y_pred),
-                'division': (lambda x: x / y_pred)}
-
-    plotter = {'plot':    (lambda x, y: ax.plot(x, y)),
-               'scatter': (lambda x, y: ax.scatter(x, y))}
+    modifier = _get_modifier(plt_mode, pred_sp)
+    plotter = _get_plotter(plotter_mode, ax)
 
     for i, q in enumerate(quantiles):
-        plotter[plotter_mode](y_pred, modifier[plt_mode](quantiles_sp[i, :]))
+        plotter(pred_sp, modifier(quantiles_sp[i, :]), label=f"{q}-quantile")
 
-    ax.legend([f"{q}-quantile" for q in quantiles])
+    plotter(pred_sp, mean_sp, label="mean", c='r')
+
+    ax.legend()
 
 
 def plot_recall(y_act: np.array, y_pred: np.array, quantiles: list[float] = None,
